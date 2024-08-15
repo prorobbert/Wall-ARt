6db12b33b3af0083c8a40becdd71b9afdcdb334c
Robbert Ruiter <robbert.ruiter@wearetriple.com>
2024-08-15 14:27:11 +0200

    Moved reload sample data to profile page

--- Wall AR-t/Views/Home/HomePage.swift
+++ Wall AR-t/Views/Home/HomePage.swift
@@ -39,23 +39,23 @@ struct HomePage<Store: ArtworksStore>: View {
             .withPageDestination()
             .trackScreen(Analytics(screen: .home))
             .environmentObject(navigationStore)
-            .toolbar {
-                Button {
-                    isReloadPresented = true
-                } label: {
-                    Label("", systemImage: "arrow.clockwise")
-                        .help("Reload sample data")
-                }
-            }
-            .alert("Reload sample data?", isPresented: $isReloadPresented) {
-                if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
-                    Button("Yes, but nothing will happen", role: .destructive) {}
-                } else {
-                    Button("Yes, reload sample data", role: .destructive) {
-                        reloadSampleData()
-                    }
-                }
-            }
+//            .toolbar {
+//                Button {
+//                    isReloadPresented = true
+//                } label: {
+//                    Label("", systemImage: "arrow.clockwise")
+//                        .help("Reload sample data")
+//                }
+//            }
+//            .alert("Reload sample data?", isPresented: $isReloadPresented) {
+//                if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+//                    Button("Yes, but nothing will happen", role: .destructive) {}
+//                } else {
+//                    Button("Yes, reload sample data", role: .destructive) {
+//                        reloadSampleData()
+//                    }
+//                }
+//            }
         }
     }
 
